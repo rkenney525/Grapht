05a7e41552c2754efb59de8ebe0af51efa8bac02
vdtdev <vdtdev@gmail.com>
2016-05-16 03:51:15 -0400

    Played around with getting shaders to work on sprite renderers; added 'highlight' shader to nodes and a vanilla shader to connection using a chain texture. Created a camera control class, zoom with mouse scroll button works; panning was being weird so currently commented out.

---
+++ Assets/scripts/CameraControl.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour
+{
+
+    public float MinX = -10f, MaxX = 10f;
+    public float MinY = -5f, MaxY = -5f;
+    public float MaximumZoom = 90f, MinimumZoom = 60f;
+    private bool ortho = false;
+    private Camera cameraRef;
+    private Vector3 dragStart = new Vector2(0f, 0f);
+
+    // Use this for initialization
+    void Start()
+    {
+        this.cameraRef = this.GetComponent<Camera>();
+        this.ortho = cameraRef.orthographic;
+
+    }
+
+    void ChangePosition(Vector3 movement)
+    {
+        Vector3 newPos = this.cameraRef.transform.position + this.cameraRef.ScreenToWorldPoint(movement);
+        //Vector3 newPos = movement;
+        //Vector3 newPos = this.cameraRef.transform.(movement);
+        newPos.z = this.cameraRef.transform.position.z;
+        //   newPos *= 0.5f; // account for half the screen
+        //newPos = (newPos * 2.0f) - new Vector3(1.0f,1.0f,0.0f);
+        //newPos.z = originalZ;
+        //   newPos.x = Mathf.Min(Mathf.Max(newPos.x, MinX), MaxX);
+        //  newPos.y = Mathf.Min(Mathf.Max(newPos.y, MinY), MaxY);
+        this.cameraRef.transform.position = Vector3.Lerp(this.cameraRef.transform.position, newPos, 0.2f);
+    }
+
+    /// <summary>
+    /// Change camera FOV based on scroll delta
+    /// </summary>
+    /// <param name="delta"></param>
+    void ChangeZoom(float delta)
+    {
+        float zoomLevel = (!ortho)? this.cameraRef.fieldOfView : this.cameraRef.orthographicSize;
+        float changeAmount = (ortho) ? 0.1f : 1.0f;
+
+        float zoomChange = Mathf.Sign(delta) * changeAmount;
+        float previewZoom = zoomLevel + zoomChange;
+        
+        if (previewZoom >= MinimumZoom && previewZoom <= MaximumZoom)
+        {
+            if (ortho)
+            {
+                cameraRef.orthographicSize = previewZoom;
+            }
+            else
+            {
+                this.cameraRef.fieldOfView = previewZoom;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetMouseButtonDown(1))
+        //{
+        //    dragStart = Input.mousePosition;
+        //}
+        //if (Input.GetMouseButton(1))
+        //{
+        //    ChangePosition(dragStart - Input.mousePosition);
+        //}
+        if (Input.mouseScrollDelta.y != 0.0)
+        {
+            ChangeZoom(Input.mouseScrollDelta.y);
+        }
+    }
+}
---
+++ Assets/shaders/movable_node_unlit.shader
@@ -0,0 +1,70 @@
+﻿Shader "Unlit/movable_node_unlit"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_ang ("Angle", Float) = 5.5
+		_d ("Distance from Center", Float) = 0.170
+		_intensity ("Light Intensity", Float) = .220
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Transparent" }
+		LOD 100
+
+		Pass
+		{
+			//ZWrite Off
+			 Blend SrcAlpha OneMinusSrcAlpha 
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			// make fog work
+			//#pragma multi_compile_fog
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			float _intensity;
+			float _ang;
+			float _d;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				return o;
+			}
+			
+			float4 frag (v2f i) : SV_Target
+			{
+				float2 t = i.uv-float2(.5,.5);
+				float d2 = _d;
+				t += float2(cos(_ang)*d2,sin(_ang)*d2);
+				float c = _intensity-sqrt(t.x*t.x+t.y*t.y);
+				float4 m = tex2D(_MainTex,i.uv);
+				//float4 m=_MainTex_ST;
+				float3 cm = float3(c, c, c);	
+				//return float4(mix(cm, m.xyz, intensity), m.w);
+				float alph = m.w;
+
+				return float4((c + m.xyz)*m.a, m.a);
+			}
+			ENDCG
+		}
+	}
+}
