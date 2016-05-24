ae6c44d447e3005456b0d687a8a21941dfab6b9a
Ryan Kenney <rkenney525@gmail.com>
2016-05-24 17:35:41 -0400

    Added the correct namespace

--- Assets/scripts/Grapht/Splash/SplashControlScript.cs
+++ Assets/scripts/Grapht/Splash/SplashControlScript.cs
@@ -2,34 +2,36 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-/// <summary>
-/// Displays the Splash Screen and then loads the main menu
-/// </summary>
-public class SplashControlScript : MonoBehaviour {
-
+namespace Grapht.Splash {
     /// <summary>
-    /// The time, in milliseconds, to wait before loading the main game
+    /// Displays the Splash Screen and then loads the main menu
     /// </summary>
-    private readonly int DISPLAY_TIME = 3;
+    public class SplashControlScript : MonoBehaviour {
 
-    /// <summary>
-    /// The name of the main game scene
-    /// </summary>
-    private const string MAIN_GAME_SCENE = "MainGameScene";
+        /// <summary>
+        /// The time, in milliseconds, to wait before loading the main game
+        /// </summary>
+        private readonly int DISPLAY_TIME = 3;
 
-    /// <summary>
-    /// Kick off the waiting coroutine for the splash screen
-    /// </summary>
-    void Start() {
-        StartCoroutine(Wait());
-    }
+        /// <summary>
+        /// The name of the main game scene
+        /// </summary>
+        private const string MAIN_GAME_SCENE = "MainGameScene";
 
-    /// <summary>
-    /// Wait the specified time and then load the main menu
-    /// </summary>
-    /// <returns>The instruction to wait</returns>
-    private IEnumerator Wait() {
-        yield return new WaitForSeconds(DISPLAY_TIME);
-        SceneManager.LoadScene(MAIN_GAME_SCENE);
+        /// <summary>
+        /// Kick off the waiting coroutine for the splash screen
+        /// </summary>
+        void Start() {
+            StartCoroutine(Wait());
+        }
+
+        /// <summary>
+        /// Wait the specified time and then load the main menu
+        /// </summary>
+        /// <returns>The instruction to wait</returns>
+        private IEnumerator Wait() {
+            yield return new WaitForSeconds(DISPLAY_TIME);
+            SceneManager.LoadScene(MAIN_GAME_SCENE);
+        }
     }
 }
