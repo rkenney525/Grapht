1b0ab339735adeed04a5f3c75c71e55a020629c6
Ryan Kenney <rkenney525@gmail.com>
2017-01-17 22:11:29 -0500

    Added a hint button and a stub script for it

---
+++ Assets/scripts/Grapht/Component/HintButtonScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Grapht.Component {
+    public class HintButtonScript : MonoBehaviour {
+
+        // Use this for initialization
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+}
